6c3efeba68e30dddda130d4bb693215f06422ef5
nazgual <nazgual@ukr.net>
2019-10-12 15:55:09 +0300

    Lab01: + labels group and name + pady to labels properties - redundant commentary

--- Lab01/Lab01.py
+++ Lab01/Lab01.py
@@ -4,26 +4,23 @@ root = Tk()
 root.title("AboutForm")
 root.geometry("400x300")
 
-'''
-text_h1 = Text(width=50, height=2)
-text_h1.pack()
-text_h1.insert(1.0, "Hello world!")
-text_h1.tag_add('title', 1.0, '1.end')
-text_h1.tag_config('title', font=("Verdana", 24, 'bold'), justify=CENTER)
-'''
-lbl1 = Label(root, text="", bg="orange red", fg="white", font="none 24 bold")
-lbl1.config(anchor=CENTER)
-lbl1.pack()
-
-label_h1 = Label(root, text="Лабораторная работа №1")
-label_h1.config(font=("Verdana", 18, 'bold'), anchor=CENTER)
+label_h1 = Label(root, text="Лабораторна робота №1")
+label_h1.config(font=("Verdana", 16, 'bold'), justify=CENTER, pady=15)
 label_h1.pack()
+
+label_group_number = Label(root, text="Виконав студент групи ЗПІ 91")
+label_group_number.config(font=("Verdana", 10), justify=CENTER, pady=10)
+label_group_number.pack()
+
+label_name = Label(root, text="Івашко Сергій Володимирович")
+label_name.config(font=("Verdana", 16), justify=CENTER, pady=10)
+label_name.pack()
 
 
 def close_window():
     root.destroy()
 
-button = Button(text="Выход", command=close_window).place(x=300, y=250, width=60)
+button = Button(text="Вихід", command=close_window).place(x=300, y=250, width=60)
 
 root.mainloop()
 
