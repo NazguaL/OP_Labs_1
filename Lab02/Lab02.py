f75e4b8da76846b006355c2ed8dc01dbee3755f2
nazgual <nazgual@ukr.net>
2020-01-24 21:20:27 +0200

    Lab02: imports refactored

--- Lab02/Lab02.py
+++ Lab02/Lab02.py
@@ -5,10 +5,7 @@
 
 
 import subprocess
-from termcolor import colored
 import colorama
-from colorama import Back as bg
-from colorama import Fore as fr
 from colorama import Fore, Back, Style
 
 if subprocess.check_output('xset q | grep LED', shell=True)[65] == 50:
@@ -17,20 +14,11 @@ if subprocess.check_output('xset q | grep LED', shell=True)[65] == 51:
     capslock = True
 # https://stackoverflow.com/questions/13129804/python-how-to-get-current-keylock-status
 print("capslock ON is : ", capslock)
-print(colored('hello', 'red'), colored('world', 'green'))
-
-
 
 colorama.init()
-print(bg.RED)
-print(bg.RED)
-print(fr.RED)
-print(colorama.ansi.clear_screen())
-
+print(Back.RED)
 
 print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
+print(Style.BRIGHT + Back.GREEN + Fore.RED + 'and with a green background' + Style.RESET_ALL)
 print('back to normal now')
 
